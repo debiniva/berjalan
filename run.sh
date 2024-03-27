65c1d53edcbfbaf69d42001b6d1ace529412fc05
debini <165168267+debiniva@users.noreply.github.com>
2024-03-27 20:11:57 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e8bf7b68-c8f2-42be-a4c4-0c4e2893412a/)
 
 
 echo "$curl_output" >> Update.tsx
